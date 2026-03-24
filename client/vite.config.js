13acff9bb163af2405f53519c30680f06ffa74ad
Vasili <btriantafyllids45@gmail.com>
2026-03-24 13:53:18 +0100

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WorkoutTracker/',
+  base: '/WorkoutTracker/',  // <-- must match repo name exactly
   plugins: [react()],
   build: {
-    outDir: '../docs'  // <-- build directly into docs/
+    outDir: '../docs'         // <-- outputs directly to docs folder
   }
 })
