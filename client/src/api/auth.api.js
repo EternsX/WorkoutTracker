4295ade6e569fa4428d03b6fbd24a1df9ce10d38
Vasili <btriantafyllids45@gmail.com>
2026-03-24 15:50:50 +0100

    attempting to create the github page

--- client/src/api/auth.api.js
+++ client/src/api/auth.api.js
@@ -1,6 +1,7 @@
 import { BASE_URL } from "./base_url";
 const API_URL = `${BASE_URL}/auth`;
-
+console.log(import.meta.env)
+console.log(BASE_URL)
 export const userUrl = `${API_URL}/user`;
 export const loginUrl = `${API_URL}/login`;
 export const registerUrl = `${API_URL}/register`;
