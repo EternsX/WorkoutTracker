4b1b408050f97479678b478111801317408a78e4
Vasili <btriantafyllids45@gmail.com>
2026-03-24 17:03:40 +0100

    attempting to create the github page

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import './src/config/env.js';
 import app from './src/app.js';
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+const PORT = process.env.PORT || 3000; // fallback to 3000 for local dev
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
