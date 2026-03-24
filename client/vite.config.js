783821477a359c9e579e0785569032259ab87d9b
Vasili <btriantafyllids45@gmail.com>
2026-03-24 17:56:03 +0100

    attempting to create the github page

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/WorkoutTracker/',  // <-- must match repo name exactly
   plugins: [react()],
   build: {
-    outDir: '../docs'         // <-- outputs directly to docs folder
+    outDir: 'build' // change from 'docs' to 'build' if you prefer
   }
 })
