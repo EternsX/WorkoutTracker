5b013ab3b390c2b80a36b82df8065b6234967c33
Vasili <btriantafyllids45@gmail.com>
2026-03-25 06:54:59 +0100

    attempting to deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import './src/config/env.js';
 import app from './src/app.js';
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
